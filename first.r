6411fa225eebfea275dba40a74d050eb6202cd6a
Muhammad Tayyab <tayyabmuhammad459@gmail.com>
2020-12-15 04:58:56 +0500

    new

--- first.r
+++ first.r
@@ -7,3 +7,17 @@ b<- 3
 sum=a+b
 print(sum)
 
+m=matrix(6:11,nrow = 3,ncol = 3)
+
+m=6:11
+dim(m)<-c(6,1)
+ m
+x=12:17
+cbind(m,x)
+d<-factor(c("monday","tuesday",'wed','thur','ffri','sat','sun'),level=c("monday","tuesday",'wed','thur','ffri','sat','sun'))
+d
+table(d)
+unclass(d)
+
+d
+
